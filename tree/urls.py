c624ce9740137c39d55553e976366cce3fbd2163
jfeldman777 <jfeldman777@gmail.com>
2018-10-31 22:01:34 +0300

    roles

--- tree/urls.py
+++ tree/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('i_cur/', views.i_cur, name='i_cur'),
+    path('i_par/', views.i_par, name='i_par'),
+    path('i_tea/', views.i_tea, name='i_tea'),
+    path('i_stu/', views.i_stu, name='i_stu'),
+    path('i_gue/', views.i_gue, name='i_gue'),
+
     path('', views.index, name='index'),
 ]
--- tree/views.py
+++ tree/views.py
@@ -3,4 +3,19 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request,'index.html')    
+    return render(request,'index.html')
+
+def i_cur(request):
+    return render(request,'i_cur.html')
+
+def i_par(request):
+    return render(request,'i_par.html')
+
+def i_stu(request):
+    return render(request,'i_stu.html')
+
+def i_tea(request):
+    return render(request,'i_tea.html')
+
+def i_gue(request):
+    return render(request,'i_gue.html')                       
