f0372519efa057c24c8e6ba32975aea1c5035315
jfeldman777 <jfeldman777@gmail.com>
2018-11-03 17:16:18 +0300

    tree

--- tree/urls.py
+++ tree/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views, v_cur
 
 urlpatterns = [
+    path('add_item/<int:id>/<int:location>/', v_cur.add_item),
     path('subtree/<int:id>/', v_cur.subtree, name='subtree'),
     path('tree_nav/<int:id>/', views.tree_nav, name='tree_nav'),
     path('change_txt/<int:id>/', v_cur.change_txt, name='change_txt'),
--- tree/v_cur.py
+++ tree/v_cur.py
@@ -1,9 +1,58 @@
 from django.shortcuts import render, get_object_or_404
 from .models import MagicNode
+from django import forms
 from django.forms import ModelForm
+from django.contrib.auth.models import User
 
 def msg(request,msg):
     return render(request, 'msg.html', {'msg': msg})
+
+class AddItemForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    location = forms.IntegerField(widget=forms.HiddenInput())
+
+class UnameForm(forms.Form):
+    uname = forms.SlugField(required = False)
+
+def add_item(request,id,location):
+    old_node = MagicNode.objects.get(id=id)
+    owner = request.user
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = AddItemForm(request.POST)
+        form2 = UnameForm(request.POST)
+        if form2.is_valid():
+            uname = form2.cleaned_data['uname']
+            if uname == '':
+                uname = 'jacobfeldman'
+            owner = User.objects.get(username = uname)
+        # check whether it's valid:
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            location = int(form.cleaned_data['location'])
+
+            new_node = MagicNode(desc=name,owner=owner)
+            if location == 1:
+                old_node.add_sibling('left',instance=new_node)
+            elif location == 2:
+                old_node.add_sibling('right',instance=new_node)
+            else:
+                old_node.add_child(instance=new_node)
+
+            return msg(request,'child created')
+        else:
+            return msg(request,'cannnot create child')
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = AddItemForm(initial={'location':location})
+        form2 = UnameForm()
+
+        return render(request, 'add_item.html',
+            {'form': form,
+            'form2':form2,
+            'old_node':old_node,
+            'location':location,
+            })
 
 def subtree(request,id):
     node = MagicNode.get_first_root_node()
@@ -13,7 +62,6 @@ def subtree(request,id):
     return render(request,'tree.html',
                     {'annotated_list':annotated_list,
                      })
-
 
 def tree(id):
     children = []
